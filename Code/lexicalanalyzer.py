0e2da944d3ec3e99823ad400556ecd01557e99a2
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-03 21:28:58 +0900

    Upload mid-ver

--- Code/lexicalanalyzer.py
+++ Code/lexicalanalyzer.py
@@ -13,66 +13,236 @@ class LexicalAnalyzer(object):
     ASSIGN = ['=']
     TERM = [';']
     COMMA = [',']
-    MERGE = VARIABLE + KEYWORD + LOGIC + BRACE + PAREN + TERM + COMMA
+    MERGE = BRACE + PAREN + TERM + COMMA + OPERATOR + COMPARISON
     LETTER = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q,' 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', \
-    'a', 'b', 'c' 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    SYMBOL = ['&', '|', '+', '-', '/', '<', '>', '"', '.', '_']
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    SYMBOL = ['&', '|', '+', '-', '/', '<', '>', '"', '.', '_', '!'] + BRACE + PAREN + ASSIGN + TERM + COMMA
     ZERO = ['0']
     NON_ZERO = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     DIGIT = ZERO + NON_ZERO
-    ALPHABET = LETTER + DIGIT + SYMBOL + WHITESPACE + BRACE + PAREN + ASSIGN + TERM + COMMA
+    ALPHABET = LETTER + DIGIT + SYMBOL + WHITESPACE
 
-    input_strem = None
+    input_stream = None
 
     def __init__(self):
         pass
 
     def __init__(self, file):
-        self.input_strem = file
+        self.input_stream = file
 
-    def id_dfa(self, string):
-        symbol = LETTER + ZERO + NON_ZERO + ['_']
+    def id_dfa(self, input_string, char):
+        sub_string = ""
+        symbol = self.LETTER + self.ZERO + self.NON_ZERO + ['_']
+        final = [1, 2, 3, 4, 5, 6]
+        i = 0; j = 0
+        transition_table = [[1, -1, -1, 2], [3, 4, 5, 6], [3, 4, 5, 6], [3, 4, 5, 6], \
+        [3, 4, 5, 6], [3, 4, 5, 6], [3, 4, 5, 6]]
+
+        if char == "":
+            char = self.input_stream.read(1)
+
+        # Read string
+        if char in symbol:
+            input_string = input_string + char
+            char = self.input_stream.read(1)
+            while char in symbol:
+                input_string = input_string + char
+                char = self.input_stream.read(1)
+
+        if char not in self.WHITESPACE + self.BRACE + self.PAREN + self.ASSIGN:
+            input_string = input_string + char
+            char = ""
+
+        # Analyze
+        for c in input_string:
+            if c in self.LETTER: j = 0
+            elif c in self.ZERO: j = 1
+            elif c in self.NON_ZERO: j = 2
+            elif c in ['_']: j = 3
+            else:
+                return sub_string, False, char
+
+            tmp_i = i
+            i = transition_table[i][j]
+            sub_string = sub_string + c
+
+            if i==-1:
+                i = tmp_i
+                break
+        
+        if i in final:
+            return sub_string, True, char
+        else:
+            return sub_string, False, char
+
+
+
+    def string_dfa(self, input_string, char):
+        sub_string = ""
+        symbol = self.LETTER + self.ZERO + self.NON_ZERO + [' ', '"']
+        i = 0; j = 0
+        final = [2]
+        transition_table = [[1, -1, -1, -1, -1], [2, 3, 4, 5, 6], [-1, -1, -1, -1, -1],\
+        [2, 3, 4, 5, 6], [2, 3, 4, 5, 6], [2, 3, 4, 5, 6] ]
+
+        if char == "":
+            char = self.input_stream.read(1)
+
+        # Read string
+        if char in symbol:
+            input_string = input_string + char
+            char = self.input_stream.read(1)
+            while char in symbol:
+                input_string = input_string + char
+                char = self.input_stream.read(1)
+
+        # Analyze
+        for c in input_string:
+            if c in '"': j = 0
+            elif c in self.LETTER: j = 1
+            elif c in self.ZERO: j = 2
+            elif c in self.NON_ZERO: j = 3
+            elif c in ['_']: j = 4
+
+
+            tmp_i = i
+            i = transition_table[i][j]
+            sub_string = sub_string + c
+
+            if i==-1:
+                i = tmp_i
+                break
+        
+        if i in final:
+            return sub_string, True, char
+        else:
+            return sub_string, False, char
+
+
 
 
 
     def run(self):
+        flag = True
         line_num = 1
         sub_string = ""
         symbol_table = []
 
-        for i in range(1, 9):
-            c = self.input_strem.read(1)
+        while (True):
+        #for i in range(1, 72):
+            if flag:
+                c = self.input_stream.read(1)
+
+            if c=="":
+                break
+            
+            flag = True
+
             if c not in self.ALPHABET:
                 print("Wrong character, Line: ", line_num)
                 exit()
 
+            # Check the line
+            if c == '\n':
+                line_num = line_num + 1
+                c = ""
+                continue
+
+            # Ignore thre white space
+            if c in self.WHITESPACE:
+                c = ""
+                continue
+
 #$$$$$$$$$
 # Core part
-            if c in self.WHITESPACE:
-                continue
-
             sub_string = sub_string + c
-            
-            # Variable, Keyword, LOGIC, BRACE, PAREN, TERM, COMMA
-            if sub_string in self.MERGE:
-                symbol_table.append(sub_string)
-                sub_string = ""
-                continue
-            # OPERATOR
-            if sub_string in self.OPERATOR:
-                symbol_table.append(sub_string)
-                sub_string = ""
-                continue
-            # ASSIGN
-            if sub_string in self.ASSIGN:
-                symbol_table.append(sub_string)
-                sub_string = ""
-                continue
-            # ID
-            
-#$$$$$$$$$$$$$
+            c = ""
 
+            # Variable, Keyword, Logic
+            if sub_string in self.LETTER:
+                c = self.input_stream.read(1)
+                while (c in self.LETTER):
+                    sub_string = sub_string + c
+                    c = self.input_stream.read(1)
+                if sub_string in self.VARIABLE + self.KEYWORD + self.LOGIC:
+                    symbol_table.append(sub_string)
+                    sub_string = ""
+                    flag = False
+                    continue
+                
+
+            # BRACE, PAREN, TERM, COMMA, OPERATOR, COMPARISON
+            if sub_string in self.MERGE:
+                if sub_string in ['<', '>']:
+                    if c == "":
+                        c = self.input_stream.read(1)
+                    if c == sub_string:
+                        symbol_table.append(sub_string + c)
+                        sub_string = ""
+                        c = ""
+                        continue
+                    elif sub_string+c in self.COMPARISON:
+                        symbol_table.append(sub_string + c)
+                        sub_string = ""
+                        c = ""
+                        continue
+
+                symbol_table.append(sub_string)
+                sub_string = ""
+                continue
+
+            # ASSIGN, OPERATOR
+            if sub_string in self.ASSIGN:
+                c = self.input_stream.read(1)
+                # Comparison ==
+                if sub_string + c in self.COMPARISON:
+                    symbol_table.append(sub_string + c)
+                    sub_string = ""
+                    c = ""
+                    continue
+                # ASSIGN
+                else:
+                    symbol_table.append(sub_string)
+                    sub_string = ""
+                    flag = False
+                    continue
+
+            # ID
+            if sub_string[0] in self.LETTER + ['_']:
+                sub_string, fact, c = self.id_dfa(sub_string, c)
+                if fact:
+                    symbol_table.append(sub_string)
+                    sub_string = ""
+                    if c != "":
+                        flag = False
+                        continue
+                    else:
+                        flag = True
+                        continue
+                else:
+                    print("Line", line_num, ": Wrong input stream - Identifier")
+                    exit()
+
+            # String
+            if sub_string[0] == '"':
+                sub_string, fact, c = self.string_dfa(sub_string, c)
+                if fact:
+                    symbol_table.append(sub_string)
+                    sub_string = ""
+                    if c != "":
+                        flag = False
+                        continue
+                    else:
+                        flag = True
+                        continue
+                else:
+                    print("Line", line_num, ": Wrong input stream - String")
+                    exit()
+                    
         print(symbol_table)
+
+        return 0
+
         #while (True):
         #    # Check the alphabet
         #    # Check the whitespace
