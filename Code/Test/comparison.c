b39425f5888a19652fd7bec8be7df8df0e640a39
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-08 00:34:37 +0900

    Upload test case

---
+++ Code/Test/comparison.c
@@ -0,0 +1,83 @@
+void main() {
+    int a = 0;
+    int b = 5;
+    bool c;
+
+    c = i<j;
+    c = i <j;
+    c = i< j;
+    c = i < j;
+    c =i<j;
+    c =i <j;
+    c =i< j;
+    c =i < j;
+    c=i<j;
+    c=i <j;
+    c=i< j;
+    c=i < j;
+
+    c = i>j;
+    c = i >j;
+    c = i> j;
+    c = i > j;
+    c =i>j;
+    c =i >j;
+    c =i> j;
+    c =i > j;
+    c=i>j;
+    c=i >j;
+    c=i> j;
+    c=i > j;
+
+    c = i==j;
+    c = i ==j;
+    c = i== j;
+    c = i == j;
+    c =i==j;
+    c =i ==j;
+    c =i== j;
+    c =i == j;
+    c=i==j;
+    c=i ==j;
+    c=i== j;
+    c=i == j;
+
+    c = i!=j;
+    c = i !=j;
+    c = i!= j;
+    c = i != j;
+    c =i!=j;
+    c =i !=j;
+    c =i!= j;
+    c =i != j;
+    c=i!=j;
+    c=i !=j;
+    c=i!= j;
+    c=i != j;
+    
+    c = i<=j;
+    c = i <=j;
+    c = i<= j;
+    c = i <= j;
+    c =i<=j;
+    c =i <=j;
+    c =i<= j;
+    c =i <= j;
+    c=i<=j;
+    c=i <=j;
+    c=i<= j;
+    c=i <= j;
+
+    c = i>=j;
+    c = i >=j;
+    c = i>= j;
+    c = i >= j;
+    c =i>=j;
+    c =i >=j;
+    c =i>= j;
+    c =i >= j;
+    c=i>=j;
+    c=i >=j;
+    c=i>= j;
+    c=i >= j;
+}
