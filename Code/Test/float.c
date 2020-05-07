14f8e9641ccc8af5de3830398fc9733f8119ba63
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-07 23:13:14 +0900

    Upload test case

---
+++ Code/Test/float.c
@@ -0,0 +1,9 @@
+void main() {
+    float a = 0.0;
+    float b = 0.0123456789;
+    float c = 0.0000000001;
+    float d = 1.0;
+    float e = 1234567890.0;
+    float f = 1.0123456789;
+    float g = 1.0000000001;
+}
