dd1908b9cddac8850917ce51dee5c7b979484b39
heesang1996 <rhs1208gm@gmail.com>
2020-06-26 20:17:40 +0900

    rules, terminal, nontermianl list add

--- Syntax_Analyzer/syntax_analyzer.py
+++ Syntax_Analyzer/syntax_analyzer.py
@@ -3,6 +3,53 @@ import os
 
 class SyntaxAnalyzer(object):
 
+    #terminals
+    TERMINALS = ['vtype','num','float','literal','id','if','else','while','for',
+                 'return','addsub','multdiv','assign','comp','semi','comma',
+                 'lparen','rparen','lbrace','rbrace']
+    
+    #non-terminals
+    NON_TERMINALS = ['CODE','VDECL','FDECL','ARG','MOREARGS','BLOCK','STMT','ASSIGN'
+                     ,'RHS','EXPR','TERM','FACTOR','COND','RETURN','ELSE']
+
+    #epsilon
+    EPSILON = ['epsilon']
+
+    #rules
+    RULES = ['S → CODE',
+             'CODE → VDECL CODE',
+             'CODE → FDECL CODE',
+             'CODE → epsilon',
+             'VDECL → vtype id semi',
+             'VDECL → vtype ASSIGN semi',
+             'ASSIGN → id assign RHS',
+             'FDECL → vtype id lparen ARG rparen lbrace BLOCK RETURN rbrace',
+             'ARG → vtype id MOREARGS',
+             'ARG → epsilon',
+             'MOREARGS → comma vtype id MOREARGS',
+             'MOREARGS → epsilon',
+             'BLOCK → STMT BLOCK',
+             'BLOCK → epsilon',
+             'STMT → VDECL',
+             'STMT → ASSIGN semi',
+             'STMT → if lparen COND rparen lbrace BLOCK rbrace ELSE',
+             'STMT → while lparen COND rparen lbrace BLOCK rbrace',
+             'STMT → for lparen ASSIGN semi COND semi ASSIGN rparen lbrace BLOCK rbrace',
+             'ELSE → else lbrace BLOCK rbrace',
+             'ELSE → epsilon',
+             'RHS → EXPR',
+             'RHS → literal',
+             'EXPR → TERM addsub EXPR',
+             'EXPR → TERM',
+             'TERM → FACTOR multdiv TERM',
+             'TERM → FACTOR',
+             'FACTOR → lparen EXPR rparen',
+             'FACTOR → id',
+             'FACTOR → num',
+             'FACTOR → float',
+             'COND → FACTOR comp FACTOR',
+             'RETURN → return FACTOR semi']
+
     # Variables
     file = None         # input text file
     terminal_list = []     # input terminal
@@ -17,14 +64,20 @@ class SyntaxAnalyzer(object):
             terminal = line.split()[0]
             self.terminal_list.append(terminal)
 
-            # Print for debugging
-            print(terminal)
+            #Print for debugging
+            #print(terminal)
 
     def run(self):
         # Read file
         self.readFile()
-
-
+        #Print for debugging
+        print(self.terminal_list)
+        #Print for debugging
+        print(len(self.TERMINALS))
+        #Print for debugging
+        print(len(self.NON_TERMINALS))
+        #Print for debugging
+        print(len(self.RULES))
         return True
 
 # Main function
