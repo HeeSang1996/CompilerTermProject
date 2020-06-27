6d29f13943e09bd5b93d89b780fcd176b64a8465
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-06-27 16:42:32 +0900

    Upload test case

---
+++ Syntax_Analyzer/TestCase/correct/code.c
@@ -0,0 +1,21 @@
+int main(int arc){
+
+    int num1 = 0;
+    int num2 = 0;
+    int tmp = 10;
+    char welcom = "hello world";
+
+    if( a > b){
+        tmp = tmp/2;
+        for(i = 0 ; i < 10; i = i + 1){
+            i = i*i;
+        }
+    }
+    else{
+        while(num2<10){
+            num2 = num2 + 1;
+        }
+    }
+
+    return 0;
+}
